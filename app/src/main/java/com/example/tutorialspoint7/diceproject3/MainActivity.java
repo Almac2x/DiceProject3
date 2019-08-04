8d21cc4253b3362f637df0580ef749196880d612
Almac2x <53673507+Almac2x@users.noreply.github.com>
2019-08-04 17:27:10 +0800

    Working

--- app/src/main/java/com/example/tutorialspoint7/diceproject3/MainActivity.java
+++ app/src/main/java/com/example/tutorialspoint7/diceproject3/MainActivity.java
@@ -2,15 +2,60 @@ package com.example.tutorialspoint7.diceproject3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView dice;
+    Image image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("Test");
+        dice = (ImageView)findViewById(R.id.dice);
+        dice.setImageResource(R.drawable.dice_1);
+
+
+
+
+
+
+    }
+
+    public void Roll(View view){
+
+        int side = (int)(Math.random()*6)+1; //6 Max 1 Min
+
+        switch(side){
+            case 1:
+                dice.setImageResource(R.drawable.dice_1);
+                break;
+            case 2:
+                dice.setImageResource(R.drawable.dice_2);
+                break;
+            case 3:
+                dice.setImageResource(R.drawable.dice_3);
+                break;
+            case 4:
+                dice.setImageResource(R.drawable.dice_4);
+                break;
+            case 5:
+                dice.setImageResource(R.drawable.dice_5);
+                break;
+            case 6:
+                dice.setImageResource(R.drawable.dice_6);
+                break;
+
+        };
+
+
+
+
     }
 }
